c57601a7c3c7ed557979c1cb3f16ec934d672f03
Dip Paul <266556095+dp02xd@users.noreply.github.com>
2026-08-27 02:00:38 +0530

    Refactor build.sh for fog configuration and user update
    Updated build script to use new configuration files and user.

--- build.sh
+++ build.sh
@@ -4,10 +4,13 @@
 # Copyright (C) 2020-2021 Adithya R.
 
 SECONDS=0 # builtin bash timer
-ZIPNAME="vauxite-$(date '+%Y%m%d-%H%M')-spes.zip"
+ZIPNAME="NoName-fog-$(date '+%Y%m%d-%H%M').zip"
 TC_DIR="$(pwd)/tc/clang-r450784e"
 AK3_DIR="$(pwd)/android/AnyKernel3"
-DEFCONFIG="vendor/spes-perf_defconfig"
+
+BASE_DEFCONFIG="vendor/bengal-perf_defconfig"
+FOG_CONFIG="vendor/xiaomi/fog.config"
+KSU_CONFIG="vendor/ksu.config"
 
 if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
    head=$(git rev-parse --verify HEAD 2>/dev/null); then
@@ -15,7 +18,7 @@ if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
 fi
 
 export PATH="$TC_DIR/bin:$PATH"
-export KBUILD_BUILD_USER=nobody
+export KBUILD_BUILD_USER=dp02xd
 export KBUILD_BUILD_HOST=android-build
 
 if ! [ -d "$TC_DIR" ]; then
@@ -26,26 +29,30 @@ if ! [ -d "$TC_DIR" ]; then
 	fi
 fi
 
-if [[ $1 = "-r" || $1 = "--regen" ]]; then
-	make O=out ARCH=arm64 $DEFCONFIG savedefconfig
-	cp out/defconfig arch/arm64/configs/$DEFCONFIG
-	echo -e "\nSuccessfully regenerated defconfig at $DEFCONFIG"
-	exit
-fi
-
-if [[ $1 = "-rf" || $1 = "--regen-full" ]]; then
-	make O=out ARCH=arm64 $DEFCONFIG
-	cp out/.config arch/arm64/configs/$DEFCONFIG
-	echo -e "\nSuccessfully regenerated full defconfig at $DEFCONFIG"
-	exit
-fi
-
 if [[ $1 = "-c" || $1 = "--clean" ]]; then
 	rm -rf out
 fi
 
 mkdir -p out
-make O=out ARCH=arm64 $DEFCONFIG
+
+echo -e "\nMerging configuration fragments for fog..."
+MAKE_ARGS="ARCH=arm64" ARCH=arm64 ./scripts/kconfig/merge_config.sh -O out \
+	"arch/arm64/configs/$BASE_DEFCONFIG" \
+	"arch/arm64/configs/$FOG_CONFIG" \
+	"arch/arm64/configs/$KSU_CONFIG" || exit 1
+
+if [[ $1 = "-r" || $1 = "--regen" ]]; then
+	make O=out ARCH=arm64 savedefconfig
+	cp out/defconfig arch/arm64/configs/vendor/fog-perf_defconfig
+	echo -e "\nSuccessfully saved merged defconfig to arch/arm64/configs/vendor/fog-perf_defconfig"
+	exit 0
+fi
+
+if [[ $1 = "-rf" || $1 = "--regen-full" ]]; then
+	cp out/.config arch/arm64/configs/vendor/fog-perf_defconfig
+	echo -e "\nSuccessfully saved full combined config to arch/arm64/configs/vendor/fog-perf_defconfig"
+	exit 0
+fi
 
 echo -e "\nStarting compilation...\n"
 make -j$(nproc --all) O=out ARCH=arm64 LLVM=1 LLVM_IAS=1 Image.gz dtb.img dtbo.img 2> >(tee log.txt >&2) || exit $?
@@ -55,9 +62,9 @@ dtb="out/arch/arm64/boot/dtb.img"
 dtbo="out/arch/arm64/boot/dtbo.img"
 
 if [ -f "$kernel" ]; then
-	echo -e "\nKernel compiled succesfully! Zipping up...\n"
+	echo -e "\nKernel compiled successfully! Zipping up...\n"
 	if [ -d "$AK3_DIR" ]; then
-		cp -r $AK3_DIR AnyKernel3
+		cp -r "$AK3_DIR" AnyKernel3
 	elif ! git clone -q https://github.com/CHRISL7/AnyKernel3 -b master; then
 		echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
 		exit 1
@@ -75,4 +82,3 @@ else
 	echo -e "\nCompilation failed!"
 	exit 1
 fi
-
