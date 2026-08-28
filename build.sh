521c72ac5b30d84def4638f44347acaef8c15c20
Dip Paul <266556095+dp02xd@users.noreply.github.com>
2026-08-29 00:13:49 +0530

    prepare build.sh

--- build.sh
+++ build.sh
@@ -4,18 +4,16 @@
 # Copyright (C) 2020-2021 Adithya R.
 
 SECONDS=0 # builtin bash timer
-ZIPNAME="vauxite-$(date '+%Y%m%d-%H%M')-spes.zip"
+ZIPNAME="AETHER.XXKSU.FOG.$(date '+%d%m%Y%H%M').zip"
 TC_DIR="$(pwd)/tc/clang-r450784e"
 AK3_DIR="$(pwd)/android/AnyKernel3"
-DEFCONFIG="vendor/spes-perf_defconfig"
 
-if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
-   head=$(git rev-parse --verify HEAD 2>/dev/null); then
-	ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8).zip"
-fi
+BASE_DEFCONFIG="vendor/bengal-perf_defconfig"
+FOG_CONFIG="vendor/xiaomi/fog.config"
+KSU_CONFIG="vendor/ksu.config"
 
 export PATH="$TC_DIR/bin:$PATH"
-export KBUILD_BUILD_USER=nobody
+export KBUILD_BUILD_USER=dp02xd
 export KBUILD_BUILD_HOST=android-build
 
 if ! [ -d "$TC_DIR" ]; then
@@ -26,44 +24,58 @@ if ! [ -d "$TC_DIR" ]; then
 	fi
 fi
 
-if [[ $1 = "-r" || $1 = "--regen" ]]; then
-	make O=out ARCH=arm64 $DEFCONFIG savedefconfig
-	cp out/defconfig arch/arm64/configs/$DEFCONFIG
-	echo -e "\nSuccessfully regenerated defconfig at $DEFCONFIG"
-	exit
-fi
-
-if [[ $1 = "-rf" || $1 = "--regen-full" ]]; then
-	make O=out ARCH=arm64 $DEFCONFIG
-	cp out/.config arch/arm64/configs/$DEFCONFIG
-	echo -e "\nSuccessfully regenerated full defconfig at $DEFCONFIG"
-	exit
-fi
-
 if [[ $1 = "-c" || $1 = "--clean" ]]; then
 	rm -rf out
 fi
 
 mkdir -p out
-make O=out ARCH=arm64 $DEFCONFIG
+
+curl -LSs "https://raw.githubusercontent.com/backslashxx/KernelSU/refs/heads/master/kernel/setup.sh" | bash
+
+echo -e "\nMerging configuration fragments for fog..."
+MAKE_ARGS="ARCH=arm64" ARCH=arm64 ./scripts/kconfig/merge_config.sh -O out \
+	"arch/arm64/configs/$BASE_DEFCONFIG" \
+	"arch/arm64/configs/$FOG_CONFIG" \
+	"arch/arm64/configs/$KSU_CONFIG" || exit 1
+
+if [[ $1 = "-r" || $1 = "--regen" ]]; then
+	make O=out ARCH=arm64 savedefconfig
+	cp out/defconfig arch/arm64/configs/vendor/fog-perf_defconfig
+	echo -e "\nSuccessfully saved merged defconfig to arch/arm64/configs/vendor/fog-perf_defconfig"
+	exit 0
+fi
+
+if [[ $1 = "-rf" || $1 = "--regen-full" ]]; then
+	cp out/.config arch/arm64/configs/vendor/fog-perf_defconfig
+	echo -e "\nSuccessfully saved full combined config to arch/arm64/configs/vendor/fog-perf_defconfig"
+	exit 0
+fi
 
 echo -e "\nStarting compilation...\n"
-make -j$(nproc --all) O=out ARCH=arm64 LLVM=1 LLVM_IAS=1 Image.gz dtb.img dtbo.img 2> >(tee log.txt >&2) || exit $?
+# 1. Build Image.gz, individual DTBs (dtbs), and dtbo.img
+make -j$(nproc --all) O=out ARCH=arm64 LLVM=1 LLVM_IAS=1 Image.gz dtbs dtbo.img 2> >(tee log.txt >&2) || exit $?
+
+# 2. Concatenate individual compiled DTBs into a single dtb image file
+# (Qualcomm Bengal/Fog DTB output path)
+if [ -d "out/arch/arm64/boot/dts/vendor/qcom" ]; then
+    cat out/arch/arm64/boot/dts/vendor/qcom/*.dtb > out/arch/arm64/boot/dtb
+elif [ -d "out/arch/arm64/boot/dts/qcom" ]; then
+    cat out/arch/arm64/boot/dts/qcom/*.dtb > out/arch/arm64/boot/dtb
+fi
 
 kernel="out/arch/arm64/boot/Image.gz"
-dtb="out/arch/arm64/boot/dtb.img"
+dtb="out/arch/arm64/boot/dtb"
 dtbo="out/arch/arm64/boot/dtbo.img"
 
-if [ -f "$kernel" ]; then
-	echo -e "\nKernel compiled succesfully! Zipping up...\n"
+if [ -f "$kernel" ] && [ -f "$dtb" ]; then
+	echo -e "\nKernel compiled successfully! Zipping up...\n"
 	if [ -d "$AK3_DIR" ]; then
-		cp -r $AK3_DIR AnyKernel3
-	elif ! git clone -q https://github.com/CHRISL7/AnyKernel3 -b master; then
+		cp -r "$AK3_DIR" AnyKernel3
+	elif ! git clone -q https://github.com/Redmi-Fog/AnyKernel3; then
 		echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
 		exit 1
 	fi
-	cp $kernel $dtb $dtbo AnyKernel3
-	rm -rf out/arch/arm64/boot
+	cp "$kernel" "$dtb" "$dtbo" AnyKernel3/
 	cd AnyKernel3
 	git checkout master &> /dev/null
 	zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
@@ -72,7 +84,6 @@ if [ -f "$kernel" ]; then
 	echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
 	echo "Zip: $ZIPNAME"
 else
-	echo -e "\nCompilation failed!"
+	echo -e "\nCompilation failed! Missing kernel or dtb output."
 	exit 1
 fi
-
